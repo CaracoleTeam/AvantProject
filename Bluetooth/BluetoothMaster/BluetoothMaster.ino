d9939129dd7494096e2dd70412ffd45d649ddd62
João Victor Pessoa <jvpessoa10@hotmail.com>
2018-10-15 00:16:39 -0300

    Bluetooths ja transmitem dados e serializa

--- Bluetooth/BluetoothMaster/BluetoothMaster.ino
+++ Bluetooth/BluetoothMaster/BluetoothMaster.ino
@@ -1,8 +1,13 @@
 #include <SoftwareSerial.h>
 
 
+typedef struct passo{
+    float stepTime;
+    float stepLenght;
+}Passo;
+
 SoftwareSerial COM(10,11);
-char value;
+
 
 void setup(){
   COM.begin(9600);  
@@ -10,9 +15,23 @@ void setup(){
 }
 
 void loop(){
+  
+  
   if(COM.available()>0){
-    value = (char) COM.read();
+    Passo passoEsquerdo;
+    switch((char)COM.read()){
+      case 't':
+        passoEsquerdo.stepTime =COM.read();
+        break;
+      case 'l':
+        passoEsquerdo.stepLenght = COM.read();
+        break;
+        
+    }
+    Serial.println(passoEsquerdo.stepLenght);
+    
   }
-  Serial.println(value);
+  
+  
   
 }
--- Bluetooth/BluetoothSlave/BluetoothSlave.ino
+++ Bluetooth/BluetoothSlave/BluetoothSlave.ino
@@ -2,11 +2,19 @@
 
 SoftwareSerial COM(9,10);
 
+
 void setup(){
   COM.begin(9600);
 }
 
 void loop(){
-  COM.write('1');
+
+  int t = 3;
+  int l = 5;
+  
+  COM.write("t");
+  COM.write(t);
+  COM.write("l");
+  COM.write(l);
   delay(1000);
 }
